60bdbde792be2536d5547a0dd42ac3f64fd20795
Kristof Coomans <kristof@2dotstwice.be>
2015-08-26 15:48:21 +0200

    III-197: Add OrganizerImporterInterface and PlaceImporterInterface

---
+++ src/Organizer/OrganizerImporterInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\UDB2\Organizer;
+
+use CultuurNet\UDB3\Organizer\Organizer;
+
+/**
+ * Imports organizers from UDB2 (where they are called 'actors') into UDB3.
+ */
+interface OrganizerImporterInterface
+{
+    /**
+     * @param string $organizerId
+     * @return Organizer|null
+     */
+    public function updateOrganizerFromUDB2($organizerId);
+
+    /**P
+     * @param string $organizerId
+     * @return Organizer|null
+     */
+    public function createOrganizerFromUDB2($organizerId);
+}
