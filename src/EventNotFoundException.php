e3418b232aab0c86692567449ccb8a7c4375cba2
Bert Ramakers <bertramakers@me.com>
2016-11-18 16:51:12 +0100

    III-1625: Removed repository classes that sync back to entry api and import events/places/organizers on load(), plus related classes.

--- src/EventNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CultuurNet\UDB3\UDB2;
-
-use CultuurNet\UDB3\Event\EventNotFoundException as UDB3EventNotFoundException;
-
-class EventNotFoundException extends UDB3EventNotFoundException
-{
-}
