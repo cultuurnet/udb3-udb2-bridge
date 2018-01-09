eb4c09bc98182e8b8c44310e623c8749ca89cac3
Luc Wollants <luc.wollants@apptite.be>
2018-01-09 09:32:58 +0100

    III-2345 Avoid same cdbid validation error when location and organiser have both a cdbid of null.

--- test/Event/EventXMLValidatorServiceTest.php
+++ test/Event/EventXMLValidatorServiceTest.php
@@ -32,4 +32,22 @@ class EventXMLValidatorServiceTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * @test
+     */
+    public function does_not_throw_for_location_and_organizer_with_null_cdbid()
+    {
+        $eventXmlValidatorService = new EventXMLValidatorService(
+            new StringLiteral('http://www.cultuurdatabank.com/XMLSchema/CdbXSD/3.3/FINAL')
+        );
+
+        $eventXml = file_get_contents(__DIR__ . '/samples/event_with_null_value_for_cdbid_location_and_organizer.xml');
+
+        $expected = [];
+
+        $actual = $eventXmlValidatorService->validate($eventXml);
+
+        $this->assertEquals($expected, $actual);
+    }
 }
